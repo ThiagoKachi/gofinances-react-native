cae838c8321a8cf6c660ba047980e098b8b6c321
Thiago Kachinsky <thiagokachidev@gmail.com>
2023-01-17 14:03:14 -0300

    create header

--- src/components/screens/Dashboard/index.tsx
+++ src/components/screens/Dashboard/index.tsx
@@ -3,11 +3,25 @@ import React from 'react';
 import * as S from './styles';
 
 export function Dashboard() {
-  return (
+  return ( 
     <S.Container>
-      <S.Title>
-        Dashboard
-      </S.Title>
+      <S.Header>
+        <S.UserWrapper>
+
+          <S.UserInfo>
+            <S.Photo
+              source={{ uri: 'https://avatars.githubusercontent.com/u/61670495?v=4' }}
+
+            />
+            <S.User>
+              <S.UserGreeting>Olá,</S.UserGreeting>
+              <S.UserName>Thiago</S.UserName>
+            </S.User>
+          </S.UserInfo>
+
+          <S.Icon name="power" />
+        </S.UserWrapper>
+      </S.Header>
     </S.Container>
   )
 }
